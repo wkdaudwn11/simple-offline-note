699105c2aa604f42ebdf5a021293b0d625ad9429
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-03-15 21:57:14 +0900

    #4. Note Query part One

--- src/clientState.js
+++ src/clientState.js
@@ -1,34 +1,43 @@
-export const defaults = {
-  notes: []
-};
+import { VariablesAreInputTypes } from "graphql/validation/rules/VariablesAreInputTypes";
 
+export const defaults = {
+  notes: [
+    {
+      __typename: "Note",
+      id: 1,
+      title: "Title",
+      content: "Content"
+    }
+  ]
+};
 export const typeDefs = [
   `
-  schema {
-    query: Query
-    mutation: Mutation
+  extend schema {
+      query: Query
+      mutation: Mutation
   }
-
-  type Query {
-    notes: [Note]!
-    note(id: Int): Note
+  extend type Query {
+      notes: [Note]!
+      note(id: Int!): Note
   }
-
-  type Mutation {
-    createNote(title: String!, contents: String!)
-    editNote(id: String!, title: String!, content: String!)
+  type Mutation{
+      createNote(title: String!, content: String!): Note
+      editNote(id: String!, title: String!, content:String!): Note
   }
-
-  type Note {
-    id: Int!
-    title: String!
-    content: String!
+  type Note{
+      id: Int!
+      title: String!
+      content: String!
   }
   `
 ];
-
 export const resolvers = {
   Query: {
-    notes: () => true
+    note: (_, variables, { getCacheKey }) => {
+      // variables => 파라미터로 받은 변수들 모음
+      const id = getCacheKey({ __typename: "Note", id: variables.id });
+      console.log(id);
+      return null;
+    }
   }
 };
